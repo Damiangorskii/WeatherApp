ef476f1c32947cda542fe46f319e2ae501ab3b83
Damian <damian.gorski1@wp.pl>
2020-11-07 20:48:08 +0100

    Detail city information added

--- WeatherApp/weather/urls.py
+++ WeatherApp/weather/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import home
+from .views import home_view, detail_city_weather_view
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', home_view, name='home'),
+    path('<city>', detail_city_weather_view, name='detail-city-weather'),
 ]
--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -7,7 +7,7 @@ import datetime
 
 # Create your views here.
 
-def home(request):
+def home_view(request):
 
     query = request.GET.get('q')
     
@@ -22,7 +22,7 @@ def home(request):
         
 
         weather_data = {
-            'city': city,
+            'city': r['name'],
             'country': r['sys']['country'],
             'temperature': round((int(r['main']['temp'])-273.15),1),
             'temperatureFeelsLike': round((int(r['main']['feels_like'])-273.15),1),
@@ -38,3 +38,38 @@ def home(request):
 
         return render(request, 'weather/city_weather.html', context)
     return render(request, 'home.html', {})
+
+
+def detail_city_weather_view(request, city):
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=d15a8e835c366afc687227da39ceb337'
+    r = requests.get(url.format(city)).json()
+        
+
+    weather_data = {
+        'city': r['name'],
+        'country': r['sys']['country'],
+        'temperature': round((int(r['main']['temp'])-273.15),1),
+        'temperatureFeelsLike': round((int(r['main']['feels_like'])-273.15),1),
+        'mainDescription': r['weather'][0]['main'],
+        'icon': r['weather'][0]['icon'],
+        'datetime': datetime.datetime.now(),
+        'pressure': r['main']['pressure'],
+        'description': r['weather'][0]['description'],
+        'humidity': r['main']['humidity'],
+        'visibility': r['visibility'],
+        'tempMin': r['main']['temp_min'],
+        'tempMax': r['main']['temp_max'],
+        'wind': r['wind']['speed'],
+        'cloudiness': r['clouds']['all'],
+        'sunrise': r['sys']['sunrise'],
+        'sunset': r['sys']['sunset'],
+        'timezone': r['timezone'],
+        }
+        
+    context = {
+            'weather_data': weather_data,
+    }
+
+
+    return render(request, 'weather/detail_city_weather.html', context)
